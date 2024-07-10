1202f299abe9ce77958505c79e63c7bcd29bafef
maicotreinmuller <maicotreinmuller@gmail.com>
2024-07-10 20:24:02 -0300

    Delete scripts directory

--- scripts/backupbutton.js
+++
@@ -1,2 +0,0 @@
-<a href="download/crlv/${placa.trim()}.pdf" class="button-download" download>Download</a>
-<a href="download/crlv/${placa.trim()}.pdf" class="button-visualizar" target="_blank">Visualizar</a>
